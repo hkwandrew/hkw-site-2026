486b7dbd7edb929fc6c4c69885aef66702c5a312
hkwandrew <142352529+hkwandrew@users.noreply.github.com>
2026-05-27 14:29:56 -0400

    Updates

---
+++ assets/usePageActive-D3vBqxE2.js
@@ -0,0 +1,1 @@
+import{x as e,y as t}from"./index-D60mxNkW.js";var n=e(t(),1),r=()=>{let[e,t]=(0,n.useState)(!1);return(0,n.useEffect)(()=>{let e=requestAnimationFrame(()=>t(!0));return()=>cancelAnimationFrame(e)},[]),e};export{r as t};
